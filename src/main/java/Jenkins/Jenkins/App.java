18c9caaa6a7d58e3cf26328eac57d9145da88bfe
Rahul R <rrahulnair397@gmail.com>
2026-05-11 21:25:10 +0530

    first commit

---
+++ src/main/java/Jenkins/Jenkins/App.java
@@ -0,0 +1,10 @@
+package Jenkins.Jenkins;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/day1/LoginTest.java
@@ -0,0 +1,11 @@
+package day1;
+
+import org.testng.annotations.*;
+
+public class LoginTest {
+  @Test
+	void hello()
+   {
+	   System.out.println("Login test is working");
+   }
+}
